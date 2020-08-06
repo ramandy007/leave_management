2e0b1da433859479961ba0a18d64760ae3a2b030
unknown <mahalaxmi.muthuvel@gmail.com>
2020-08-07 01:08:11 +0530

    Path Fixing In Test Module

--- faculty/Testing/SeleniumTesting/src/Testing/LoginTesting.java
+++ faculty/Testing/SeleniumTesting/src/Testing/LoginTesting.java
@@ -1,5 +1,4 @@
 package Testing;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,14 +11,15 @@ public class LoginTesting
 		String at;
 		System.setProperty("webdriver.chrome.driver","src\\Testing\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
-		//driver.get("https://ramandy007.github.io/leave_management/faculty/login.html");
-		driver.get("file:///E:/B%20Tech%20AMRITA/7th%20Semester/NCP/leave_management/faculty/login.html");
+		driver.get("https://ramandy007.github.io/leave_management/faculty/login.html");
+		//driver.get("file:///E:/B%20Tech%20AMRITA/7th%20Semester/NCP/leave_management/faculty/login.html");
 		driver.manage().window().maximize();
 		driver.findElement(By.id("un")).sendKeys(tcun);
 		driver.findElement(By.id("pw")).sendKeys(tcpw);
 		driver.findElement(By.id("sb")).click();
 		at=driver.getTitle();
 		String et="Faculty Dashboard";
+		System.out.println(at);
 		if(at.equalsIgnoreCase(et))
 		{
 			System.out.println("Sucessful Test");
